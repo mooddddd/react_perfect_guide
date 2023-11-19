8e5a4ad19ab9347a8183e54fe1e1ea7ec566de52
mood <mooddddd@naver.com>
2023-11-19 22:12:24 +0900

    feat: 섹션3, 45강까지 정리

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>2023.11.19</div>
+      <div>
+        <h2>Car insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
